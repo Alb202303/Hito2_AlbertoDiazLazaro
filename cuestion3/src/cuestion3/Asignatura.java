ee567a6f83ce927f95b945b52a3373d14edb96dd
AlberFP <93380153+AlberFP@users.noreply.github.com>
2021-12-15 00:43:03 +0100

    Add files via upload

---
+++ cuestion3/src/cuestion3/Asignatura.java
@@ -0,0 +1,21 @@
+package cuestion3;
+
+public class Asignatura {
+	private String nombre;
+
+	public Asignatura(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+
+}
---
+++ cuestion3/src/cuestion3/Nota.java
@@ -0,0 +1,21 @@
+package cuestion3;
+
+public class Nota {
+	private float Calificación;
+
+	public Nota(float calificación) {
+		super();
+		Calificación = calificación;
+	}
+
+	public float getCalificación() {
+		return Calificación;
+	}
+
+	public void setCalificación(float calificación) {
+		Calificación = calificación;
+	}
+	
+	
+	
+}
